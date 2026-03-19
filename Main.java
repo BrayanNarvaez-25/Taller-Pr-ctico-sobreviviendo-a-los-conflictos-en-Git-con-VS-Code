2c9e359acb6f9819f21de4b784095836656308f3
EDWINCSN <c.edwin32@hotmail.com>
2026-03-18 23:06:15 -0500

    fase3 integrante 2 y 4

--- Main.java
+++ Main.java
@@ -1,11 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         BilleteraVirtual miBilletera = new BilleteraVirtual("Juan Perez", 100.0);
 
+        System.out.println("=== BIENVENIDO AL SISTEMA BANCARIO ===");
+       
         miBilletera.mostrarSaldo();
 
         miBilletera.transferir(20.0);
 
         miBilletera.mostrarSaldo();
+
+         // scaner
+        Scanner montoTrasferir=new Scanner(System.in);
+        //ingresar dato
+        System.out.println("¿Cuánto desea transferir?");
+        double monto=montoTrasferir.nextDouble();
+        miBilletera.transferir(monto);
+
+
     }
 }
