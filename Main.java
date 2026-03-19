97f60552b97336378e43afd10f792951fdc581ae
BrayanNarvaez-25 <brayann251107g@gmail.com>
2026-03-18 23:06:35 -0500

    fase 3: integrante 1 y 3

--- Main.java
+++ Main.java
@@ -1,10 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         BilleteraVirtual miBilletera = new BilleteraVirtual("Juan Perez", 100.0);
+        BilleteraVirtual miBilletera2 = new BilleteraVirtual("María Lopez", 500.0);
 
         miBilletera.mostrarSaldo();
+        miBilletera2.mostrarSaldo();
 
         miBilletera.transferir(20.0);
+        miBilletera.transferir(30.0);
+        miBilletera.transferir(40.0);
 
         miBilletera.mostrarSaldo();
     }
