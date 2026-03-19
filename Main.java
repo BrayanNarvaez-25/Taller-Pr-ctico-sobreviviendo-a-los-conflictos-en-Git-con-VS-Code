8df03ec9d6a230c7fc8d400755a55c1d6aa00d57
BrayanNarvaez-25 <brayann251107g@gmail.com>
2026-03-18 23:08:22 -0500

    Fase 3: integrante 1 y 3

--- Main.java
+++ Main.java
@@ -22,7 +22,7 @@ public class Main {
         System.out.println("¿Cuánto desea transferir?");
         double monto=montoTrasferir.nextDouble();
         miBilletera.transferir(monto);
-
+        montoTrasferir.close();
 
     }
 }
