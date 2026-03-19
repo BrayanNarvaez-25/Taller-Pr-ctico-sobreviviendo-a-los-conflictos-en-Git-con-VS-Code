26437ccac9b4d5dadd0c690f47468f01cdc5640e
BrayanNarvaez-25 <brayann251107g@gmail.com>
2026-03-18 21:59:26 -0500

    Modifico atributos de Billetera

--- Billetera.java
+++ Billetera.java
@@ -1,17 +1,18 @@
 public class Billetera {
-    double saldo;
+    double balanceTotal;
     String titular;
+    boolean cuentaActiva = true;
 
-    public Billetera(String titular, double saldoInicial) {
+    public Billetera(String titular, double balanceTotal) {
         this.titular = titular;
-        this.saldo = saldoInicial;
+        this.balanceTotal = balanceTotal;
     }
 
     public void transferir(double monto) {
-        saldo = saldo - monto;
+        balanceTotal = balanceTotal - monto;
     }
 
     public void mostrarSaldo() {
-        System.out.println("Saldo de " + titular + ": $" + saldo);
+        System.out.println("Saldo de " + titular + ": $" + balanceTotal);
     }
 }
